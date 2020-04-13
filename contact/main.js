e1f3d31593dcb8e5dd205f96aee5dab2356a21af
Javier Calderon <javierjcjr@gmail.com>
2020-04-12 23:06:06 -0700

    contact form operation 1

--- contact/main.js
+++ contact/main.js
@@ -40,3 +40,42 @@ sr.reveal('.animate-bottom',{
     distace:'25rem',
     delay: 600
 });
+
+function validation(){
+    var first = document.getElementById("first").value;
+    var last = document.getElementById("last").value;
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var error_message = document.getElementById("error_message");
+    var text;
+    
+    error_message.style.padding = "10px";
+    if(first.length < 1){
+        text = "Please Enter Valid First Name";
+        error_message.innerHMTL = text;
+        return false;
+    }
+    if(last.length < 1){
+        text = "Please Enter Valid Last Name";
+        error_message.innerHMTL = text;
+        return false;
+    }
+    if(isNaN(phone) || phone.length !== 10){
+        text = "Please Enter Valid Phone Number";
+        error_message.innerHMTL = text;
+        return false;
+    }
+    if(email.indexOf("@") !== -1 || email.length < 8){
+        text = "Please Enter Valid email";
+        error_message.innerHMTL = text;
+        return false;
+    }
+    if(message.length < 50){
+        text = "Please Enter More Than 50 Characters";
+        alert(text);
+        return false;
+    }
+    alert("form Submitted Successfully!")
+    return true;
+}
